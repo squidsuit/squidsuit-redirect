2abd7cf9b5295e14f851748430997c3c2e111ae9
Robert Bell <rmbbell@gmail.com>
2021-06-14 03:03:27 +0000

    Changed backend from S3 to Terraform Cloud

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket = "cloudlab-terraform"
-    key    = "backend/dns-forwarder/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-    dynamodb_table = "terraform"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Ua"
+
+    workspaces {
+      name = "tf-url-redirect"
+    }
   }
 }
