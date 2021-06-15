767b2ff554361abdfdda0a8709ad7249b3bdaa61
Robert Bell <rmbbell@gmail.com>
2021-06-15 00:27:47 +0000

    Added KMS to the s3 buckets.

--- s3.tf
+++ s3.tf
@@ -20,6 +20,14 @@ resource "aws_s3_bucket" "ssbucket" {
         target_prefix = "log/${each.value}/"
     }
 
+    server_side_encryption_configuration {
+        rule {
+        apply_server_side_encryption_by_default {
+            kms_master_key_id = aws_kms_key.mykey.arn
+            sse_algorithm     = "aws:kms"
+            }
+        }
+    }
 }
 
 resource "aws_s3_bucket" "logbucket" {
@@ -30,4 +38,19 @@ resource "aws_s3_bucket" "logbucket" {
         enabled = true
     }
 
+    server_side_encryption_configuration {
+        rule {
+        apply_server_side_encryption_by_default {
+            kms_master_key_id = aws_kms_key.mykey.arn
+            sse_algorithm     = "aws:kms"
+            }
+        }
+    }
+
+}
+
+resource "aws_kms_key" "mykey" {
+  description             = "Encrypt those bucket's contents."
+  deletion_window_in_days = 14
+  enable_key_rotation = true
 }
